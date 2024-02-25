14ec132416a60172a3097fb98bf3dd4576292e52
TheRealKaup <ethan.kauf736@gmail.com>
2024-02-25 08:59:40 +0200

    Added comments to

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,11 @@
+-- Create a workspace that its projects will be built in `build/`
 workspace "KTech"
 	configurations { "Release" }
 	location "build"
 
+-- Include the KTech library
 include "ktech"
 
+-- Build the examples, they are configured so object files will be in `build/obj/<project_name>` and binary files will be in `build/bin/` 
 include "examples/simpleplatform1"
 include "examples/widgetstest"
