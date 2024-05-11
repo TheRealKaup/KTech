d7f488d0bed9d4909ee03644cc256fcdaf68193b
TheRealKaup <ethan.kauf736@gmail.com>
2024-05-11 11:34:19 +0300

    Inlined `Engine::Engine()` and created `Engine::Quit()`. Close #88.

--- ktech/engine/engine.hpp
+++ ktech/engine/engine.hpp
@@ -41,6 +41,11 @@ public:
 	Output output;
 	Time time;
 
-	Engine(UPoint imageSize, int16_t ticksPerSecondLimit = 24)
+	inline Engine(UPoint imageSize, int16_t ticksPerSecondLimit = 24)
 		: collision(this), input(this), output(this, imageSize), time(this, ticksPerSecondLimit) {}
+
+	inline void Quit()
+	{
+		running = false;
+	}
 };
