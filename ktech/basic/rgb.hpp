64fa1a75802edc2c35da9bce45ce42a6d5bf2ff8
TheRealKaup <ethan.kauf736@gmail.com>
2024-12-04 14:48:43 +0200

    Document `RGB`.

--- ktech/basic/rgb.hpp
+++ ktech/basic/rgb.hpp
@@ -24,20 +24,29 @@
 #include "../ktech.hpp"
 #undef KTECH_DEFINITION
 
+//! 24-bit color, able of representing 16,777,216 (2^24) different colors.
 struct KTech::RGB
 {
-	uint8_t r, g, b;
+	uint8_t r; //!< Red primary color.
+	uint8_t g; //!< Green primary color.
+	uint8_t b; //!< Blue primary color.
 
+	/*!
+		@brief Construct an `RGB` color.
+		@param [in] red Red primary color.
+		@param [in] green Green primary color.
+		@param [in] blue Blue primary color.
+	*/
 	constexpr RGB(uint8_t red = 0, uint8_t green = 0, uint8_t blue = 0)
 		: r(red), g(green), b(blue) {}
 
+	/*!
+		@brief Compare 2 `RGB`s.
+		@param [in] rgb The `RGB` to compare with this `RGB`.
+		@return `true`: the red, green and blue primary colors are equal. `false`: they are unequal.
+	*/
 	constexpr auto operator==(const RGB& rgb) const -> bool
 	{
 		return r == rgb.r && g == rgb.g && b == rgb.b;
-	}
-
-	constexpr auto operator!=(const RGB& rgb) const -> bool
-	{
-		return !(*this == rgb);
 	}
 };
