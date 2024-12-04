af29084b2cc590108f406cefea0ab84c29beb64a
TheRealKaup <ethan.kauf736@gmail.com>
2024-12-04 14:05:42 +0200

    Document `Cell`.

--- ktech/basic/cell.hpp
+++ ktech/basic/cell.hpp
@@ -26,22 +26,30 @@
 #include "rgb.hpp"
 #include "../utility/rgbcolors.hpp"
 
+/*!
+	Terminal cell comprising `RGB` foreground (character) color, `RGB` background color, and an ASCII character.
+*/
 struct KTech::Cell
 {
-	char c;
-	RGB f;
-	RGB b;
+	char c; //!< ASCII character.
+	RGB f; //!< Foreground (character) color.
+	RGB b; //!< Background color.
 
+	/*!
+		@brief Construct a `Cell`.
+		@param character ASCII character.
+		@param foreground Foreground (character) color.
+		@param background Background color.
+	*/
 	constexpr Cell(char character = ' ', RGB foreground = RGBColors::black, RGB background = RGBColors::black)
 		: c(character), f{foreground}, b(background) {}
 
+	/*!
+		@brief Compare 2 `Cell`s.
+		@return `true`: the foreground colors, background colors, and characters are equal. `false`: they are unequal.
+	*/
 	constexpr auto operator==(const Cell& cell) const -> bool
 	{
 		return (c == cell.c) && (f == cell.f) && (b == cell.b);
-	}
-
-	constexpr auto operator!=(const Cell& cell) const -> bool
-	{
-		return !(*this == cell);
 	}
 };
