3d01c5f5fe5d6e24184f26a2eb5f88789a25ee12
TheRealKaup <ethan.kauf736@gmail.com>
2024-12-04 22:29:36 +0200

    Document `Frame`.

--- ktech/widgets/frame.hpp
+++ ktech/widgets/frame.hpp
@@ -22,23 +22,39 @@
 
 #include "../ktech.hpp"
 
+/*!
+	@brief Widget that looks like a frame.
+*/
 class Frame : public KTech::Widget
 {
 public:
-	KTech::RGBA m_unselectedRGBA, m_selectedRGBA;
+	/*!
+		@brief Construct a `Frame`.
 
+		@param engine Parent `Engine`.
+		@param ui `UI` to immediately enter.
+		@param position World position.
+		@param size Frame's size.
+		@param unselected Foreground (text and frame) color set when `Button` is unselected.
+		@param selected Foreground color set when `Button` is selected.
+	*/
 	Frame(KTech::Engine& engine,
 		KTech::ID<KTech::UI> ui,
-		KTech::Point pos,
+		KTech::Point position,
 		KTech::UPoint size,
-		KTech::RGBA unselectedRGBA = KTech::RGBAColors::gray,
-		KTech::RGBA selectedRGBA = KTech::RGBAColors::white)
-		: Widget(engine, ui, pos), m_unselectedRGBA(unselectedRGBA), m_selectedRGBA(selectedRGBA)
+		KTech::RGBA unselected = KTech::RGBAColors::gray,
+		KTech::RGBA selected = KTech::RGBAColors::white)
+		: Widget(engine, ui, position), m_unselectedRGBA(unselected), m_selectedRGBA(selected)
 	{
 		// Texture
 		SetSize(size);
 	}
 
+	/*!
+		@brief Resize the frame.
+
+		@param size New size.
+	*/
 	void SetSize(KTech::UPoint size)
 	{
 		KTech::RGBA tempRGBA = m_selected ? m_selectedRGBA : m_unselectedRGBA;
@@ -53,7 +69,7 @@ public:
 		m_textures[ti_rightFrame].Simple(KTech::UPoint(1, size.y - 2), KTech::CellA('|', tempRGBA), KTech::Point(size.x - 1, 1));
 	}
 
-protected:
+private:
 	enum TextureIndex : size_t
 	{
 		ti_topLeftCorner,
@@ -66,6 +82,8 @@ protected:
 		ti_rightFrame,
 		TEXTURES_SIZE
 	};
+
+	KTech::RGBA m_unselectedRGBA, m_selectedRGBA;
 
 	void OnSelect() override
 	{
